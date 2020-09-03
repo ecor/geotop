a5ce375593b52815918ad1b1302e9570653d0027
ecor <emanuele.cordano@gmail.com>
2020-09-03 22:10:27 +0200

    SatVapPressure (Added if conditioon on temp)

--- src/geotop/meteo.cc
+++ src/geotop/meteo.cc
@@ -188,6 +188,8 @@ double SatVapPressure(double T, double P)
   // https://en.wikipedia.org/wiki/Clausius%E2%80%93Clapeyron_relation#cite_note-11
   // https://watermark.silverchair.com/bams-86-2-225.pdf?token=AQECAHi208BE49Ooan9kkhW_Ercy7Dm3ZL_9Cf3qfKAc485ysgAAApEwggKNBgkqhkiG9w0BBwagggJ-MIICegIBADCCAnMGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMfZbADrzR64Sek9raAgEQgIICRGWLRcBBnY2qFIWY3bSyCIUJshRez25alWui0C8DIn_d8Z8-w-F_4_lngTuI5Ak6-438_kHY7RmMBqVE8WPti7tloGd12kyAGa6NjUPAtuIyB_qHrljYMPLciUN9Y9QpaETsxEm6mneoY8uZZhaBjJlM2BISbaw0AkTe7Kgz1hyIEpdz6DL0IideUGgjLdG_baefNK_PYp2SHXpE4IkqrgmEqP1Zc5W_1lw4Gu0cXfboS7WG7Tv1pPnLQZ72Ni6unT4Z8p5OTtoiBHDdPVxcdCDR5B0FnqkesgRCyuqARlLYn8975W2DV3Yt0gZAICX0QUGy9a8iT_o7ONXbettJytGoQD5f0UiAeZK-p0Lc829rbjXuRNRUuXT-pJ0i_u4D1MGDpjYR1w28irA2qfLuj6WVxH2H7nRtIPM0OOwkGIk2F3xdqhCo2mGo6JQym_piFUk-0fHVccS0h4_TVafAXIupX6Sf8FFPXPoOuo6ufRpTdQvRlu1g4-nnnbQcd5_iMo7t000Xt725YcbRB-_dfeR-hQlVueK-uPjYiz8GNFJsY2ufB2yUMckpOtmUXoO2gvJGIHoeMk7M08gdxa6036Wcfzil2RpJStIk4Z9yuV_nKP9QusMIiQoiiPdoMCwlVeHDhte-p60XvFHIZDnENJg_L90Jm5mR8IInEljr4WsqPp7SCoNQqrW-V5JT-JbivM3--QJZ1hdgh8P13ZEHXyHqFRSmmBiyBA57hrRjtvG6YJf7NO-gWGrzdeJrDKkObAbIBXo
   // https://meteo-wagenborgen.nl/wp/2019/09/26/corrections-to-the-august-roche-magnus-equation-in-pwsfwi/
+  if (T<(-c+1)) {T=-c+1;} // EC 20200902
+
   printf("T=%f\n",T); // EC 20200902
   printf("P=%f\n",P); // EC 20200902
   e=A*exp(b*T/(c+T));
@@ -203,6 +205,7 @@ void SatVapPressure_2(double *e, double *de_dT, double T, double P)
   A=6.1121*(1.0007+3.46E-6*P);
   b=17.502;
   c=240.97;
+  if (T<(-c+1)) {T=-c+1;} // EC 20200902
   *e=A*exp(b*T/(c+T));
   *de_dT=(*e)*(b/(c+T)-b*T/pow_2(c+T));
 }
@@ -210,11 +213,13 @@ void SatVapPressure_2(double *e, double *de_dT, double T, double P)
 //Temperature from saturated water vapour
 double TfromSatVapPressure(double e, double P)
 {
-  double A, b, c;
+  double A, b, c,T;
   A=6.1121*(1.0007+3.46E-6*P);
   b=17.502;
   c=240.97;
-  return c*log(e/A)/(b-log(e/A));
+  T=c*log(e/A)/(b-log(e/A)) // EC 20200902
+  if (T<(-c+1)) {T=-c+1;} // EC 20200902
+  return T;
 }
 
 double SpecHumidity(double e, double P)
