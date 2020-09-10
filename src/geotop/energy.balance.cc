3b71b19af5085db82cebad8c5dfa67991421e588
ecor <emanuele.cordano@gmail.com>
2020-09-10 16:44:25 +0200

    Tdew test

--- src/geotop/energy.balance.cc
+++ src/geotop/energy.balance.cc
@@ -402,7 +402,7 @@ short PointEnergyBalance(long i, long r, long c, double Dt, double JDb,
     }
     printf("PointEnargyBalance Prain_over=%f\n",Prain_over); // EC 20200910
     printf("PointEnargyBalance Psnow_over=%f\n",Psnow_over); // EC 20200910
-    
+
     //Adjusting snow precipitation in case of steep slope (contribution by Stephan Gruber)
     if (A->P->snow_curv > 0 && (*A->T->slope)(r,c) > A->P->snow_smin)
     {
@@ -2458,7 +2458,7 @@ void EnergyFluxes(double t, double Tg, long r, long c, long n, // 5 parameters
     //bare soil
     if (fc<1)
     {
-        printf("start if ...") // EC 20200902
+        printf("EnergyFluxes ..."); // EC 20200902
         printf("Ta=%f\n",Ta); // EC 20200902
         printf("Tg=%f\n",Tg); // EC 20200902
         aero_resistance(zmu, zmT, z0s, d0s, rz0s, v, Ta, Tg, Qa, *Qg, P, LR, Lobukhov,
